2d95c8c1790b2331e12d111a11b432f821762d7a
jackhnguyen <119372912+jackhnguyen@users.noreply.github.com>
2025-01-26 08:32:23 -0800

    Login changed

--- app/app/login/page.js
+++ app/app/login/page.js
@@ -52,7 +52,7 @@ export default function Home() {
       <nav className="flex items-center justify-between p-5 h-16 bg-primary">
         <div className="flex gap-5 items-center">
           <a
-            href=""
+            href="https://planithacks.vercel.app/"
             className="text-xl font-bold text-secondary hover:text-blue-600"
           >
             Plan-It
